75330c3ca7aa9b92e68e2b75e67b9fea6e08695a
JiawenCRevature <jiawen142@revature.net>
2022-12-06 16:09:03 -0500

    update notes

---
+++ Java_Notes/Loops.java
@@ -0,0 +1,46 @@
+package Java_Notes;
+import java.util.ArrayList;
+
+import javax.sound.midi.Soundbank;
+public class Loops {
+    //Loops are another way of controlling the flow of your application. These are particularly useful when you
+    //need an operation to happen multiple times. The easiest way to make this happen is with a for loop
+    public static void main(String[] args) {
+      // A for loop has three basic parts: a controlling value, a condition under which the loop continues, and a change
+      //to the controlling value
+      /*you can either use your ide to stop the code or press ctr + c with the terminal selected to kill the process */
+      for(int x = 0; x <= 10; x++){
+        System.out.println(x);
+      }
+      int[] number = {1,3,5,9,15,35,46,86};
+
+      for(int i = 0; i < number.length; i++){//i++ happen at the end of the for loop cycle no matter what (++i is the same)
+        System.out.println(number[i]);
+      }
+
+      //++i change value immediately
+      int y =5;
+      System.out.println(++y);
+      System.out.println(y++);
+      System.out.println(y); 
+      //while loop
+      while(y < 10){
+        System.out.println(y);
+        y++;
+      }
+      /*For loops are particularly useful when you know how many times you need to do something, or you are working through a 
+      data collection. A while loop is going to be more useful when you can't be sure how many times an operation needs to happen,
+      or your exit condition for the loop does not lend itself well to basic mathmatics */
+      
+      do {
+        System.out.println(y);
+        y++;
+      }while (y > 10);
+
+      //import java.util.ArrayList; is important to use arraylist.
+      ArrayList<String> cars = new ArrayList<>();
+      cars.add("car");
+
+    }
+
+}
---
+++ Java_Notes/Operator.java
@@ -0,0 +1,32 @@
+package Java_Notes;
+
+public class Operator {
+    /*In Java you have a plethora of operators that can affect the way your code operates
+     * One of the main groups of operators you should familiarize yourself with are the mathmatical operators
+     * 
+     */
+    public static void main(String[] args) {
+        // = is the assignment operator: it assigns a value to a variable;
+        int add = 5 + 5;
+        int subtract = 5 - 5;
+        int multiply = 5 * 5;
+        int divide = 5 / 5 ;
+        int modulus = 5 % 5; // 0 becasue no remainder.
+        /*
+         * there are some nice quality of life mathmatical operators that are good to know: you can take one of the math
+         * operators and add the assignment operator to it in order to change the value;
+         */
+        int number = 5;
+        number = number + 5;
+        number += 5;
+        number++;
+        /*This increase value by 1 or -- to decrease value by 1.
+         * depending on whether you put the increment/decrement operator before or after your value you will either see the 
+         * change (if you put it before the value like ++x) or you won't (if you put it after like x++)
+         */
+        number = 5;
+        System.out.println(number++);// print 5 but number is change to 6;
+        number = 5;
+        System.out.println(++number);//print 6 and number is change to 6;
+    }
+}
